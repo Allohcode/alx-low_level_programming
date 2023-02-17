09f7cf2d83b3a0d8e9f1febe9e6f7c4388b0ccb3
Allohcode <allohmelas@gmail.com>
2023-02-17 15:28:42 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main -E entry point
+ * char for alphabets
+ * int for loop
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char smalp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upalp[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(smalp[i]);
+		putchar(upalp[i]);
+	}
+		putchat('\n');
+		return (0);
+}
