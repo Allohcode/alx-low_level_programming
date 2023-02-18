f92f0ec27840cb3e1fbba6612cbe1c49b450f573
Allohcode <allohmelas@gmail.com>
2023-02-17 18:36:55 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int b;
+	char small;
+
+	for (b = '0'; b <= '9'; b++)
+	putchar(b);
+
+	for (small = 'a'; small <= 'f'; small++)
+	putchar(small);
+	putchar('\n')
+
+	return (0);
+}
