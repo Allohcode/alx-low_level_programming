bac0c8b5052a8ea9104d4da51e91f5ab92d3f2c1
Allohcode <allohmelas@gmail.com>
2023-02-21 14:01:24 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: print alphabet in lowercase, 10 times
+ * Return: Void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int coun = 0;
+
+	while (count <= 9)
+	{
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	count++
+	_putchar('\n');
+	}
+}
