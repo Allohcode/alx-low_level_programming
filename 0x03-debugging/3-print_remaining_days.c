a28d571ca194a5fe9f6a50528dd0620fe85eec93
Allohcode <allohmelas@gmail.com>
2023-02-23 14:29:38 -0800

    alll

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -21,13 +21,13 @@ if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	}
 
 	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 360 - day);
+	printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid data: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
