5c01a66c2b87bd6f84bf7e86bd5251e18ed5b518
Allohcode <allohmelas@gmail.com>
2023-02-24 08:01:15 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+
+	if (c >= '65' && c <= '90')
+	{
+
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
