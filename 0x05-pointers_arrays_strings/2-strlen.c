444cc957a7c64db361c025d7ecde8ae181064dec
Allohcode <allohmelas@gmail.com>
2023-02-27 15:12:42 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - checks the length of a string
+ * @s: inpout to function _strlen
+ * Return: Alwaus 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+
+}
