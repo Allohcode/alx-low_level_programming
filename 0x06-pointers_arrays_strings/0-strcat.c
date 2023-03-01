b238322ce155b6231a0842fab9144fc884d47d43
Allohcode <allohmelas@gmail.com>
2023-03-01 01:52:55 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * _strcat - concat two strings
+ * @dest: first string input
+ * @src: second string input
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+
+	for (i = 0; dest[i] = '\0'; i++)
+	{
+	for (j = 0; src[j] = '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	src[i];
+	}
+	return (dest);
+}
