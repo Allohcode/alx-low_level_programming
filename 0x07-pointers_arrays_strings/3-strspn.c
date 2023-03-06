8040fd03c24a24f83efdd53323706dc079286ac3
Allohcode <allohmelas@gmail.com>
2023-03-06 14:52:59 -0800

    strsp

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix
+ * @s: string
+ * @accept: containes bytes that may or may not compose part of the string
+ * Return: the number of byte that compse the length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+int j;
+unsigned int length;
+
+length = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[i] != '\0' && accept[i] != s[i]; j++)
+;
+if (s[i] == accept[j])
+	length++;
+if (accept[j] == '\0')
+	return (length);
+}
+return (lenth);
+}
