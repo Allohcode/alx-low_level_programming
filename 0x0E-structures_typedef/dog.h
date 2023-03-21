fa001a3464315525cba5755c4cf75acea7628e92
Allohcode <allohmelas@gmail.com>
2023-03-21 08:01:55 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - dog data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner ofdog
+ */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} my_dog;
+#endif
