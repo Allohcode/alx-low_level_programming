70d32beab3ad6360dcd53778b850e361110f7098
Allohcode <allohmelas@gmail.com>
2023-03-21 08:49:34 -0700

    did a correction on the if block

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog -  initialize a variable of type struct dog
- * @d: input param
- * @name: name of dog
- * @age: age of dog
- * *@owner: owner of dog
- *
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
-	d = malloc(sizeof(struct dog));
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	}
 }
