87eb25adda3dec1b717f1a56c438c82b67980f91
Allohcode <Allohmelas@gmail.com>
2023-05-04 16:23:00 -0700

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int cnv = 0;
+unsigned int ui;
+int len, base;
 
 if (!b)
 return (0);
 
-for (i = 0; b[i]; i++)
-{
-if (b[i] < '0' || b[i] > '1')
-return (0);
-cvn = 2 * cvn + (b[i] - '0');
-}
+ui = 0;
 
-return (cnv);
+for (len = 0; b[len] != '\0'; len++)
+;
+
+for (len--, base = 1; len >= 0; len--, base *= 2)
+{
+if (b[len] != '0' && b[len] != '1')
+{
+return (0);
+}
+if (b[len] & 1)
+{
+ui += base;
+}
+}
+return (ui);
 }
